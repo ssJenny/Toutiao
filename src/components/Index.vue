140425c23142cd8adb71ab68b3cf501c52721de6
shixi <sissi_sx@163.com>
2017-09-30 10:45:48 +0800

    first commit

---
+++ src/components/Index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="index">
+    <header id="header">
+        <h1>今日头条</h1>
+    </header>
+  </div>
+</template>
+<script>
+  export  default {
+    name: 'index',
+  }
+</script>
+
+<style scoped lang="less" rel="stylesheet/less">
+  #header{
+    height: 88px;
+    line-height: 88px;
+    background-color: #d43d3d;
+    color: #fff;
+  }
+  h1{
+    line-height: 88px;
+    font-size: 40px;
+    text-align: center;
+    font-weight: bold;
+  }
+</style>
